a47f9d60e5dcc13f99c39aa87af031d4cd14fb65
Łukasz Kaim <kaimukasz@gmail.com>
2023-01-31 21:06:34 +0100

    Task: calculator - brak loggingu

--- modul4.py
+++ modul4.py
@@ -14,3 +14,28 @@ print("Podane slowo " + ("jest " if(czyPalindrom(word)) else "nie jest ") + "pal
 
 
 
+# ZADANIE: KALKULATOR
+
+import logging
+
+kalk = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+a = float(input("Podaj składnik 1: "))
+b = float(input("Podaj składnik 2: "))
+
+if kalk == "1":
+    logging.info('Komunikat informacyjny')
+    c = float(input("Podaj składnik 3: "))
+    print(f"Dodaję {a}, {b} i {c}")
+    print("Wynik to", a + b + c)
+elif kalk == "2":
+    print(f"Odejmuję {a} i {b} ")
+    print("Wynik to", a - b)
+elif kalk == "3":
+    c = float(input("Podaj składnik 3: "))
+    print(f"Mnożę {a}, {b} i {c}")
+    print("Wynik to", a * b * c)
+elif kalk == "4":
+    print(f"Dzielę {a} i {b} ")
+    print("Wynik to", a / b)
+else:
+    print("koniec")
